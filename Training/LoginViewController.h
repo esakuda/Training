3e533f5f6f0ab7688d264128f7d6c20caac04e5a
esakuda <eugenia.sakuda@wolox.com.ar>
2015-02-26 13:13:14 -0300

    Login y sign up
    En la clase LoginViewController hay unos comentarios para intentar más
    adelante mejorar el label de “Terms & Conditions”

---
+++ Training/LoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.h
+//  Training
+//
+//  Created by María Eugenia Sakuda on 2/23/15.
+//  Copyright (c) 2015 María Eugenia Sakuda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FormInputViewController.h"
+
+@interface LoginViewController : FormInputViewController
+
+@end
